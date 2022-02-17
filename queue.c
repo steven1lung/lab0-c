ab9631aa23bd924e825df01ddbbd01a9e413a371
steven1lung <1030steven@gmail.com>
2022-02-17 23:44:55 +0800

    Modify q_remove_tail and q_remove_head
    Check if sp pointer is NULL before copying string.

--- queue.c
+++ queue.c
@@ -116,8 +116,10 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
         return NULL;
     element_t *target = list_first_entry(head, element_t, list);
     list_del_init(&target->list);
-    strncpy(sp, target->value, bufsize - 1);
-    sp[bufsize - 1] = '\0';
+    if (sp) {
+        strncpy(sp, target->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
     return target;
 }
 
@@ -131,8 +133,10 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
         return NULL;
     element_t *target = list_last_entry(head, element_t, list);
     list_del_init(&target->list);
-    strncpy(sp, target->value, bufsize - 1);
-    sp[bufsize - 1] = '\0';
+    if (sp) {
+        strncpy(sp, target->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
     return target;
 }
 
