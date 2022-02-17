8589ea8e82ccd9a527d11708e1621f20e9d32d5d
steven1lung <1030steven@gmail.com>
2022-02-17 21:37:24 +0800

    Add q_remove_head
    Implemented removing an element_t node from head of the queue.
    
    Check if queue is NULL or empty at first, then obtain the first
    entry by using list_first_entry() macro. Remove list_head node
    safely by list_del_init() macro. Finally, copy the string of the
    removed element to *sp and return the removed element.

--- queue.c
+++ queue.c
@@ -112,7 +112,13 @@ bool q_insert_tail(struct list_head *head, char *s)
  */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *target = list_first_entry(head, element_t, list);
+    list_del_init(head->next);
+    strncpy(sp, target->value, bufsize - 1);
+    sp[bufsize - 1] = '\0';
+    return target;
 }
 
 /*
