4d3e4d5ac595ee0f9f1a3b06f8413466a8ec412a
steven1lung <1030steven@gmail.com>
2022-02-17 21:49:44 +0800

    Modified q_remove_head
    Replaced the parameter for list_del_init().
    
    Since using &target->list is better than head->next because we
    called list_first_entry() just a line before. Making the code
    more understandable.

--- queue.c
+++ queue.c
@@ -115,7 +115,7 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
     if (!head || list_empty(head))
         return NULL;
     element_t *target = list_first_entry(head, element_t, list);
-    list_del_init(head->next);
+    list_del_init(&target->list);
     strncpy(sp, target->value, bufsize - 1);
     sp[bufsize - 1] = '\0';
     return target;
